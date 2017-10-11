f1009512236620e8944de0696a1c2eb0d2ca4339
FERRARI <rodrigo.ferrari@cstb.fr>
2017-10-11 20:49:35 +0200

    Added init file to the simul subpackage

---
+++ simul/__init__.py
@@ -0,0 +1,6 @@
+from .universe import Universe
+from .world import WonderfulWorld
+from .creatures import Creature
+from .creatures import Moth
+from .creatures import Fly
+from .control import SimulationControl
