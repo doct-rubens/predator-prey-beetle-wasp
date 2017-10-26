ba9259c7a8b0c10db9fa3dc686b3e9c49beb0d13
FERRARI <rodrigo.ferrari@cstb.fr>
2017-10-27 01:19:47 +0200

    Added bayes_cost() method testing and additional csv files generated on the process

--- tests/test_default.py
+++ tests/test_default.py
@@ -7,11 +7,12 @@
 #     > simulation control
 # with the default initialisation function and performs some operations
 
-import matplotlib.pyplot as plt
+import pandas as pd
+import os
 from funcs.init_default import init_default
 
 # simulation batch parameters
-steps = 300
+steps = 10
 n_simuls = 3
 nf = 50
 nm = 100
@@ -19,18 +20,38 @@ nm = 100
 # output csv file generation params
 output_csv_dir = 'outputs'
 output_csv_name = 'simul_results'
-output_csv = 'all'     # can be 'all', 'mean' or 'none'
-output_costs = 'all'  # same as above, 'all', 'mean' or 'none'
+output_csv = 'mean'     # can be 'all', 'mean' or 'none'
+output_costs = 'mean'  # same as above, 'all', 'mean' or 'none'
+
+# data files
+densities_file = os.path.join('..', 'data', 'my_densities.csv')
+initial_pops_file = os.path.join('..', 'data', 'my_initial_pops.csv')
 
 u, w, sc, my_plotter = init_default()
 
 # run a couple of simulation batches based on the missing
 # values based on an external dataframe
-df = sc.run_some_batches(nf, nm, steps, n_simuls,
-                         output_csv=output_csv,
-                         output_costs=output_costs,
-                         output_dir=output_csv_dir,
-                         output_name=output_csv_name
-                         )
-df[['moth-living', 'fly-living']].plot()
-plt.show()
+# sc.run_some_batches(pd.read_csv(initial_pops_file),
+#                    steps, n_simuls,
+#                    output_csv=output_csv,
+#                    output_costs=output_costs,
+#                    output_dir=output_csv_dir,
+#                    output_name=output_csv_name
+#                    )
+
+# bayes cost function testing
+sample_n_moths = 12
+sample_area = 120
+n_flies_list = [500, 1000, 1500]
+success, cost = sc.bayes_cost_function(pd.read_csv(densities_file), sample_n_moths, sample_area, n_flies_list,
+                                       pd.read_csv(os.path.join(output_csv_dir, output_csv_name + '_cost.csv')))
+if success:
+    bayes_cost_file = os.path.join(output_csv_dir, output_csv_name + '_bayes_cost.csv')
+    print('bayes cost calculated!')
+    print("results saved on file '{}'".format(bayes_cost_file))
+    cost.to_csv(bayes_cost_file)
+else:
+    print('no success on calculating the bayes cost;')
+    print('the following simulations are missing:')
+    print(cost)
+    print("saving these missing initial populations on file '{}'".format(initial_pops_file))
