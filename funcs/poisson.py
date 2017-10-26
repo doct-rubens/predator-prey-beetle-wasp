f5ede64b13e8fb615cb9c5f7bb109ab850f03614
FERRARI <rodrigo.ferrari@cstb.fr>
2017-10-27 00:49:12 +0200

    Added the poisson distribution to its respective file

--- funcs/poisson.py
+++ funcs/poisson.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Python file that will implement the Poisson distribution evaluation.
+# Python file that implements the Poisson distribution as a function
 #
 
+import numpy as np
+from math import factorial
 
-def poisson():
-    pass
+
+def poisson(n, area, p):
+    return (((p * area) ** n) * np.exp(-p * area)) / factorial(n)
